2ccd6aa4e8daf8e9ac2ce460919eaa5179c8616d
Andrei.Buinski <andrei.buinski@ventionteams.com>
2024-09-30 15:05:32 +0300

    paralax background for login page was added

--- src/features/authorization/components/LoginPage/index.tsx
+++ src/features/authorization/components/LoginPage/index.tsx
@@ -2,10 +2,29 @@ import { CommonButton } from 'root/shared/components/CommonButton';
 import { SvgSelector } from 'root/shared/components/SvgSelector';
 import './styles.scss'
 import { SvgIcon } from '@mui/material';
+import { useEffect, useState } from 'react';
 
 const LoginPage = () => {
+    const [leftPosition, setLeftPosition] = useState(0);
+    const mouseListener = (event: MouseEvent) => {
+        let x = (window.innerWidth - event.pageX * 10) / 90;
+        setLeftPosition(x);
+    }
+
+    useEffect(() => {
+        document.addEventListener("mousemove", mouseListener);
+        return(() => {
+            document.removeEventListener("mousemove", mouseListener);
+        });
+    }, []);
+
     return(
         <div className="login-page">
+            <div style={{left: leftPosition}} className="lines">
+                <div className="line"></div>
+                <div className="line"></div>
+                <div className="line"></div>
+            </div>
             <div className="login-page__container">
                 <h1>Show thyself</h1>
                 <CommonButton endIcon={<SvgIcon><SvgSelector iconName='discord-icon' /></SvgIcon>}>
