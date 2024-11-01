80e08a133b594c0ad792fb375813864fc3416add
Andrei.Buinski <andrei.buinski@ventionteams.com>
2024-11-01 05:05:16 +0300

    To topbutton was added

--- src/features/events/components/GuildPage/index.tsx
+++ src/features/events/components/GuildPage/index.tsx
@@ -1,13 +1,15 @@
 import { useParams } from "react-router-dom";
 import AddIcon from '@mui/icons-material/Add';
+import KeyboardDoubleArrowUpIcon from '@mui/icons-material/KeyboardDoubleArrowUp';
 import { CommonButton } from "root/shared/components/CommonButton";
 import { CommonModal } from "root/shared/components/CommonModal";
 import { useGetGuildByIdQuery, usePostNewEventMutation, useGetEventsByGuildIdQuery } from "../../eventsApi";
 import { EventTile } from "../EventTile";
 import './styles.scss';
 import { useEffect, useState } from "react";
-import { CircularProgress, TextField } from "@mui/material";
+import { CircularProgress, Fab, TextField } from "@mui/material";
 import { useTranslation } from "react-i18next";
+import { useOnMount } from "root/shared/hooks/useOnMount";
 
 const GuildPage = () => {
     const { guildId } = useParams();
@@ -17,6 +19,22 @@ const GuildPage = () => {
     const getEventsList = useGetEventsByGuildIdQuery({ guildId: guildId }); 
     const [isCreateNewOpened, setIsCreateNewOpened] = useState(false);
     const [eventToCreate, setEventToCreate] = useState({ eventName: "" });
+    const [isScrollUpButtonShown, setIsScrollUpButtonShown] = useState(false);
+
+    useOnMount(() => {
+        const handleScroll = () => {
+            if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
+                setIsScrollUpButtonShown(true);
+            } else {
+                setIsScrollUpButtonShown(false);
+            }
+        };
+
+        document.addEventListener('scroll', handleScroll);
+        return () => {
+            document.removeEventListener('scroll', handleScroll);
+        };
+    });
 
     useEffect(() => {
         if(postNewEventResult.isSuccess){
@@ -58,6 +76,13 @@ const GuildPage = () => {
                     })
                 }
             </div>
+                <div className="guild-page__to-top-button" 
+                    style={isScrollUpButtonShown ? {opacity: 1, bottom: '8px'} : {}} 
+                    onClick={() => document.documentElement.scrollTop = 0}>
+                    <Fab color="primary">
+                        <KeyboardDoubleArrowUpIcon />
+                    </Fab>
+                </div>
             {
                 isCreateNewOpened &&
                 <CommonModal name="Create new event" 
