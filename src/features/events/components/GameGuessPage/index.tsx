f6d7ba3264a34441f32ace39c0ab9486b1dce6b2
Andrei.Buinski <andrei.buinski@ventionteams.com>
2024-12-02 14:34:15 +0300

    GameGuessPage improved

--- src/features/events/components/GameGuessPage/index.tsx
+++ src/features/events/components/GameGuessPage/index.tsx
@@ -1,9 +1,30 @@
-const GameGuessPage = () => {
+import { useParams } from "react-router-dom";
+import { useGetEventByIdQuery } from "../../eventsApi";
+import { useSelector } from "react-redux";
+import { RootState } from "root/shared/store";
+import './styles.scss';
 
+const GameGuessPage = () => {
+    const { eventId } = useParams();
+    const eventById = useGetEventByIdQuery({ eventId });
+    const userInfo = useSelector((state: RootState) => state.authorizationReducer);
+    const selectedRecord = eventById.data?.eventRecords.find(record => record.participant.id === userInfo.id);
     return(
-    <div>
+    <div className="game-guess">
+        <div className="game-guess__header">
+            {
+                eventById.data &&
+                <div className="game-guess__user-plate">
+                    <img src={selectedRecord?.toUser?.avatarUrl} className="game-guess__user-image" />
+                    {selectedRecord?.toUser?.userName}
+                </div>
+            }
+        </div>
+        <div className="game-guess__body">
+
+        </div>
     </div>
-    )   
+    );   
 }
 
 export { GameGuessPage }
