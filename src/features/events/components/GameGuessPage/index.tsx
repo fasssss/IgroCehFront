782a1b286c356e28d7cad284dd31dd9447410ee4
Andrei.Buinski <andrei.buinski@ventionteams.com>
2024-12-03 05:37:56 +0300

    custom field component was added

--- src/features/events/components/GameGuessPage/index.tsx
+++ src/features/events/components/GameGuessPage/index.tsx
@@ -3,12 +3,20 @@ import { useGetEventByIdQuery } from "../../eventsApi";
 import { useSelector } from "react-redux";
 import { RootState } from "root/shared/store";
 import './styles.scss';
+import { SearchBar } from "root/shared/components/SearchBar";
+import { useTranslation } from "react-i18next";
+import { CommonButton } from "root/shared/components/CommonButton";
+import { useState } from "react";
+import { CommonField } from "root/shared/components/CommonField";
 
 const GameGuessPage = () => {
     const { eventId } = useParams();
+    const { t } = useTranslation();
     const eventById = useGetEventByIdQuery({ eventId });
     const userInfo = useSelector((state: RootState) => state.authorizationReducer);
     const selectedRecord = eventById.data?.eventRecords.find(record => record.participant.id === userInfo.id);
+    const [isSearchInSteam, setIsSearchInSteam] = useState(true);
+
     return(
     <div className="game-guess">
         <div className="game-guess__header">
@@ -21,7 +29,35 @@ const GameGuessPage = () => {
             }
         </div>
         <div className="game-guess__body">
-
+            <div className="game-guess__body-buttons">
+                <CommonButton
+                disabled={isSearchInSteam}
+                onClick={() => setIsSearchInSteam(true)}>
+                    Search in Steam
+                </CommonButton>
+                <CommonButton
+                disabled={!isSearchInSteam}
+                onClick={() => setIsSearchInSteam(false)}>
+                    Create custom
+                </CommonButton>
+            </div>
+            {
+                isSearchInSteam ?
+                <SearchBar 
+                placeholder= {t("Search in Steam")}
+                onTyping={undefined} 
+                isFetchingResult={true} 
+                handleOptionClick={() => {}} 
+                optionsSelect={undefined} />
+                :
+                <div>
+                    <CommonField 
+                    placeholder="Enter game name..."
+                    value={""}
+                    className="game-guess__name-field"
+                    onChange={() => {}} />
+                </div>
+            }
         </div>
     </div>
     );   
---
+++ src/shared/components/CommonField/index.tsx
@@ -0,0 +1,21 @@
+import './styles.scss';
+
+const CommonField = ({placeholder, value, onChange, className}: CommonFieldProps) => {
+
+    return(
+        <div className={`common-field ${className || ""}`}>
+            <div className="common-field__field">
+                <input list='guilds' value={value} placeholder={placeholder} onChange={onChange} className='common-field__input' type="text" />
+            </div>
+        </div>
+    );
+}
+
+export type CommonFieldProps = {
+    placeholder?: string,
+    onChange: any,
+    value: string,
+    className?: string
+}
+
+export { CommonField }
