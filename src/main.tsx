d42c2fce6de7084d1dffa26716cae4ae7b8ff879
Andrei.Buinski <andrei.buinski@ventionteams.com>
2025-02-03 12:19:59 +0000

    strict mode was removed

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { ThemeProvider } from '@mui/material/styles';
@@ -12,12 +11,10 @@ import './shared/helpers/webSocketHelper';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
     <Provider store={store}>
       <ThemeProvider theme={themes}>
         <CssBaseline />
         <RouterProvider router={router} />
       </ThemeProvider>
-    </Provider>
-  </StrictMode>,
+    </Provider>,
 )
